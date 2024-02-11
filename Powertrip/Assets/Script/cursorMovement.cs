71e22afc02c48167ad6c2163ad79824b362b807f
vgmadlads4 <vgmadlads@gmail.com>
2024-02-10 23:03:11 -0500

    Need more testing for Inputs

--- Powertrip/Assets/Script/cursorMovement.cs
+++ Powertrip/Assets/Script/cursorMovement.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class cursorMovement : MonoBehaviour
 {
     Vector2 cursorPlaceOnScreen;
     ThePlayerControls playerController;
     // Start is called before the first frame update
+
+    void Awake()
+    {
+        playerController = new ThePlayerControls();
+        playerController.CursorMoveAround.CursorMoves.performed += test => cursorPlaceOnScreen = test.ReadValue<Vector2>();
+    }
+
     void Start()
     {
-        playerController.CursorMoveAround.CursorMoves.performed += test => cursorPlaceOnScreen = test.ReadValue<Vector2>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        print(cursorPlaceOnScreen);
     }
 
    void TheCursor()
